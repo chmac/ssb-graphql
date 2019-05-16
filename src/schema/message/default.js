e946c69a6a2a9ba4c426977d8dc03fe2b37d18d6
Callum Macdonald <git@callum-macdonald.com>
2019-05-16 11:20:05 +0200

    Always return a type.
    Unclear if this is the right solution, the other option would be to make the `type` field nullable which might be a better solution.

--- src/schema/message/default.js
+++ src/schema/message/default.js
@@ -12,7 +12,7 @@ const resolvers = {
   content: (msg) => JSON.stringify(msg.value.content),
   sequence: (msg) => msg.value.sequence,
   timestamp: (msg) => msg.value.timestamp,
-  type: (msg) => msg.value.content.type,
+  type: (msg) => msg.value.content.type || 'unknown',
 }
 
 module.exports = { resolvers, typeDef };
